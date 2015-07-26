2d52697ec6da5c8e184877252d8d49ba1faa4f26
karriliu <karri.liu1027@gmail.com>
2015-07-26 13:28:33 +0800

    week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## 1.set the value of the matrix
+## 2.get the value of the matrix
+## 3.set the value of the inversed matrix
+## 4.get the value of the inversed matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -20,7 +24,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inversed matrix of the special "matrix" created with the above function. 
+##However, it first checks to see if the inversed matrix has already been calculated. 
+##If so, it gets the inversed matrix from the cache and skips the computation. 
+##Otherwise, it calculates the inversed matrix of the data and sets the value of the inversed matrix in the cache via the setsolve function
 
 cacheSolve <- function(x, ...) {
   m <- x$getsolve()
